d948a451ac124d4cbe7c25b111f30f003f43fa3f
AnhPT-GitHub <phanlong315@gmail.com>
2020-06-02 05:20:57 +0700

    dcm

--- src/utils/customTransformers.ts
+++ src/utils/customTransformers.ts
@@ -2,7 +2,13 @@ import { Transform } from 'class-transformer';
 
 export const Trim = () => Transform(value => {
   if (typeof value === 'string') {
+    if (value.trim() === '') {
+      return null;
+    }
     return value.trim();
+  }
+  if (value === null) {
+    return null;
   }
 });
 
