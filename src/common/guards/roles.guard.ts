f2067471ae65a81209b9a6e765e5ecabb931d5d1
Phan T. Anh <54684536+anhphantien@users.noreply.github.com>
2020-09-05 14:52:20 +0700

    Update roles.guard.ts

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -11,8 +11,7 @@ export class RolesGuard implements CanActivate {
       ...(this.reflector.get<number[]>('roles', context.getClass()) || []),
       ...(this.reflector.get<number[]>('roles', context.getHandler()) || []),
     ];
-    const request = context.switchToHttp().getRequest();
-    const user = request.user;
+    const { user } = context.switchToHttp().getRequest();
     if (!roles.includes(user.role)) {
       throw new ForbiddenException(ERROR_CODE.ACCESS_DENIED);
     }
