b6f51b676edf2e79014a6648fce37b422340e327
anh.phan <anh.phan@savvycomsoftware.com>
2020-09-10 10:01:59 +0700

    cập nhật ngày 10-09-2020

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -13,7 +13,7 @@ const ormconfig: TypeOrmModuleOptions = {
   synchronize: true, // đồng bộ CSDL mỗi khi khởi động ứng dụng
   keepConnectionAlive: true, // không tạo kết nối mới mỗi khi khởi động lại ứng dụng
   logging: true, // hiển thị câu lệnh SQL được thực thi mỗi lần truy vấn
-  logger: config.NODE_ENV === 'production' ? 'simple-console' : 'advanced-console',
+  logger: ['development', 'production'].includes(config.NODE_ENV) ? 'simple-console' : 'advanced-console',
   migrationsRun: false, // không thực hiện migration mỗi khi ứng dụng được khởi chạy
   migrations: ['migrations/*{.ts,.js}'],
   cli: {
