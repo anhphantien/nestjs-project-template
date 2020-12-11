617a8e96dd74cf5d4221fae06a7d1d0c7e68d1cc
anh.phan <anh.phan@savvycomsoftware.com>
2020-12-11 10:52:09 +0700

    ối dồi ôi

--- src/constants/user.ts
+++ src/constants/user.ts
@@ -1,10 +1,10 @@
 export const USER = {
-  STATUS: {
-    NOT_ACTIVATED: 0,
-    ACTIVATED: 1,
-  },
   ROLE: {
     SUPER_ADMIN: 0,
     ADMIN: 1,
+  },
+  STATUS: {
+    NOT_ACTIVATED: 0,
+    ACTIVATED: 1,
   },
 };
