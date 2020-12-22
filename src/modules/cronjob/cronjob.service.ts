679b01c6093f3888c78169fcbc6e155d63fc0a60
Phan T. Anh <54684536+anhphantien@users.noreply.github.com>
2020-12-22 15:47:17 +0700

    Update cronjob.service.ts

--- src/modules/cronjob/cronjob.service.ts
+++ src/modules/cronjob/cronjob.service.ts
@@ -9,7 +9,9 @@ require('dotenv').config();
 export class CronjobService {
   @Cron('0 0 * * *') // every day at 0:00 AM
   backupDb() {
-    fs.mkdirSync(`${process.cwd()}/backup`, { recursive: true });
-    exec(`mysqldump -u root -p${process.env.DB_PASSWORD} ${process.env.DB_NAME} > backup/${process.env.DB_NAME}.sql`);
+    if (process.env.NODE_ENV === 'production') {
+      fs.mkdirSync(`${process.cwd()}/backup`, { recursive: true });
+      exec(`mysqldump -u root -p${process.env.DB_PASSWORD} ${process.env.DB_NAME} > backup/${process.env.DB_NAME}.sql`);
+    }
   }
 }
