ae6649b8c3653c224d97e3764e1bb37ea4261004
anh.phan <anh.phan@savvycomsoftware.com>
2020-12-22 15:08:48 +0700

    ối dồi ôi

--- src/modules/cronjob/cronjob.service.ts
+++ src/modules/cronjob/cronjob.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { exec } from 'child_process';
+import fs = require('fs');
 
 require('dotenv').config();
 
 @Injectable()
 export class CronjobService {
-  @Cron('0 0 * * *', { name: 'backupDatabase' }) // every day at 0:00 AM
-  async backupDatabase() {
-    exec(`mysqldump -u root -p${process.env.DB_PASSWORD} ${process.env.DB_NAME} > ${process.env.DB_NAME}.sql`);
+  @Cron('0 0 * * *') // every day at 0:00 AM
+  backupDb() {
+    fs.mkdirSync(`${process.cwd()}/backup`, { recursive: true });
+    exec(`mysqldump -u root -p${process.env.DB_PASSWORD} ${process.env.DB_NAME} > backup/${process.env.DB_NAME}.sql`);
   }
 }
