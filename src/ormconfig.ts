dd788a4c52bb08a4f27fe30266977a24f35de8cc
anh.phan <anh.phan@savvycomsoftware.com>
2021-01-29 14:57:44 +0700

    ối dồi ôi

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,5 +1,4 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import entities = require('./entities');
 
 require('dotenv').config();
 
@@ -10,16 +9,14 @@ const ormconfig: TypeOrmModuleOptions = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: Object.values(entities),
-  synchronize: process.env.NODE_ENV === 'production' ? false : true, // đồng bộ CSDL mỗi khi khởi động ứng dụng
-  keepConnectionAlive: true, // không tạo kết nối mới mỗi khi khởi động lại ứng dụng
-  logging: true, // hiển thị câu lệnh SQL được thực thi mỗi lần truy vấn
+  bigNumberStrings: false,
+  autoLoadEntities: true,
+  synchronize: process.env.NODE_ENV === 'production' ? false : true,
+  logging: true,
   logger: ['development', 'production'].includes(process.env.NODE_ENV) ? 'simple-console' : 'advanced-console',
-  migrationsRun: false, // không thực hiện migration mỗi khi ứng dụng được khởi chạy
   migrations: ['migrations/*{.ts,.js}'],
   cli: {
     migrationsDir: 'migrations',
   },
-  bigNumberStrings: false,
 };
 export = ormconfig;
