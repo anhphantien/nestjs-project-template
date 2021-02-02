0bff1fa2778c8843dd2369e35dd6254923d8a4ca
anh.phan <anh.phan@savvycomsoftware.com>
2021-02-02 15:04:38 +0700

    ối dồi ôi

--- src/database/factories/template.factory.ts
+++ src/database/factories/template.factory.ts
@@ -8,7 +8,7 @@ const { USERNAME, OTP, OTP_TTL } = TEMPLATE.KEYWORDS.TWO_FACTOR_AUTHENTICATION;
 
 define(Template, () => {
   const template = new Template();
-  template.templateCode = TEMPLATE.CODE.FORGOT_PASSWORD;
+  template.templateCode = TEMPLATE.CODE.TWO_FACTOR_AUTHENTICATION;
   template.subject = process.env.APP_NAME;
   template.content = `Hi, ${USERNAME}! Your OTP is: <strong>${OTP}</strong>. The OTP is valid for ${OTP_TTL} minute(s).`;
   return template;
--- src/database/seeds/template.seed.ts
+++ src/database/seeds/template.seed.ts
@@ -5,7 +5,7 @@ import { Factory, Seeder } from 'typeorm-seeding';
 
 export default class CreateTemplate implements Seeder {
   async run(factory: Factory) {
-    const template = await getRepository(Template).findOne({ templateCode: TEMPLATE.CODE.FORGOT_PASSWORD });
+    const template = await getRepository(Template).findOne({ templateCode: TEMPLATE.CODE.TWO_FACTOR_AUTHENTICATION });
     if (!template) {
       await factory(Template)().create();
     }
