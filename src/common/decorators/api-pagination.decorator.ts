d937cb908c0317753c59ed2f373797ab30eda8c8
Phan T. Anh <54684536+anhphantien@users.noreply.github.com>
2021-02-09 12:21:40 +0700

    Update api-pagination.decorator.ts

--- src/common/decorators/api-pagination.decorator.ts
+++ src/common/decorators/api-pagination.decorator.ts
@@ -5,6 +5,6 @@ export const ApiPagination = () => <T>(target: Object, propertyKey: string | sym
   ApiQuery({ name: 'size', type: 'number' })(target, propertyKey, descriptor);
   ApiQuery({ name: 'page', type: 'number' })(target, propertyKey, descriptor);
   ApiQuery({ name: 'keyword', type: 'string', required: false })(target, propertyKey, descriptor);
-  ApiQuery({ name: 'filter', type: 'object', required: false })(target, propertyKey, descriptor);
-  ApiQuery({ name: 'sort', type: 'object', required: false })(target, propertyKey, descriptor);
+  ApiQuery({ name: 'filter', type: 'string', required: false })(target, propertyKey, descriptor);
+  ApiQuery({ name: 'sort', type: 'string', required: false })(target, propertyKey, descriptor);
 };
