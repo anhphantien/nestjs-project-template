08014f011eece7da703698d5fd82fa765f200c1f
Peter Phan <anh.phan1@ntq-solution.com.vn>
2021-04-06 15:27:38 +0700

    ối dồi ôi

---
+++ docker.cmd
@@ -0,0 +1,23 @@
+- Build an image:
+docker build -t <repository_name>:<tag> .
+
+- Start the containers:
+docker-compose up
+
+- Stop the containers:
+docker-compose down
+
+- Access to a container:
+docker exec -it <container_name> sh
+
+- Remove an image:
+docker rmi -f <image_id>
+
+- Remove dangling images:
+docker image prune
+
+- List all images:
+docker images
+
+- List all containers:
+docker ps
