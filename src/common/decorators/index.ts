dfe0524da5347c36a00cb0baab3ad64974e0f154
Peter Phan <anh.phan1@ntq-solution.com.vn>
2021-06-09 17:48:31 +0700

    .

--- src/common/decorators/index.ts
+++ src/common/decorators/index.ts
@@ -1,4 +1,5 @@
 export * from './api-pagination.decorator';
 export * from './current-user.decorator';
+export * from './ip-address.decorator';
 export * from './pagination.decorator';
 export * from './roles.decorator';
